6230342a907cad6bd31644e5eb322c27a1a6dec3
ej9374 <109258160+ej9374@users.noreply.github.com>
2026-01-28 10:04:38 +0900

    fix: userId 받는 방식 UserPrincipal 주입으로 변경

--- src/main/java/haennihaesseo/sandoll/domain/letter/controller/LetterController.java
+++ src/main/java/haennihaesseo/sandoll/domain/letter/controller/LetterController.java
@@ -1,11 +1,15 @@
 package haennihaesseo.sandoll.domain.letter.controller;
 
+import haennihaesseo.sandoll.global.auth.principal.UserPrincipal;
 import haennihaesseo.sandoll.domain.letter.dto.*;
 import haennihaesseo.sandoll.domain.letter.service.LetterService;
 import haennihaesseo.sandoll.domain.letter.status.LetterSuccessStatus;
+import haennihaesseo.sandoll.domain.user.entity.User;
 import haennihaesseo.sandoll.global.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,15 +23,17 @@ public class LetterController {
 
     @GetMapping("/received")
     public ResponseEntity<ApiResponse<List<ReceiveLetterResponse>>> getInbox(
-            @RequestParam(name = "userId") Long userId,
+            @AuthenticationPrincipal UserPrincipal userPrincipal,
             @RequestParam(name = "status") OrderStatus orderStatus
     ) {
+        Long userId = userPrincipal.getUser().getUserId();
         List<ReceiveLetterResponse> responses = letterService.getReceivedLettersByUser(userId, orderStatus);
         return ApiResponse.success(LetterSuccessStatus.SUCCESS_201, responses);
     }
 
     @GetMapping("/{letterId}")
     public ResponseEntity<ApiResponse<LetterDetailResponse>> getLetterDetail(
+            @AuthenticationPrincipal UserPrincipal userPrincipal,
             @PathVariable(name = "letterId") Long letterId
     ){
         LetterDetailResponse response = letterService.getLetterDetailsByLetter(letterId);
@@ -36,18 +42,20 @@ public class LetterController {
 
     @PostMapping("/delete")
     public ResponseEntity<ApiResponse<Void>> deleteLetter(
-            @RequestParam(name = "userId") Long userId,
+            @AuthenticationPrincipal UserPrincipal userPrincipal,
             @RequestBody LetterDeleteRequest request
     ){
+        Long userId = userPrincipal.getUser().getUserId();
         letterService.hideLetter(userId, request.getType(), request.getLetterIds());
         return ApiResponse.success(LetterSuccessStatus.SUCCESS_203);
     }
 
     @GetMapping("/sent")
     public ResponseEntity<ApiResponse<List<SendLetterResponse>>> getOutbox(
-            @RequestParam(name = "userId") Long userId,
+            @AuthenticationPrincipal UserPrincipal userPrincipal,
             @RequestParam(name = "status") OrderStatus orderStatus
     ){
+        Long userId = userPrincipal.getUser().getUserId();
         List<SendLetterResponse> responses = letterService.getSentLettersByUser(userId, orderStatus);
         return ApiResponse.success(LetterSuccessStatus.SUCCESS_204, responses);
     }
