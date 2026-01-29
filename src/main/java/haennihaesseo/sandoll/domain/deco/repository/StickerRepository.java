7fae63e075c28dc74bfffc741a33ac3a76c72975
oosedus <seayeon0729@gmail.com>
2026-01-29 16:07:59 +0900

    refactor: 불필요한 레포지토리 제거

--- src/main/java/haennihaesseo/sandoll/domain/deco/repository/StickerRepository.java
+++
@@ -1,8 +0,0 @@
-package haennihaesseo.sandoll.domain.deco.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StickerRepository extends JpaRepository<Sticker, Long> {
-}
--- src/main/java/haennihaesseo/sandoll/domain/font/repository/UserFontRepository.java
+++
@@ -1,8 +0,0 @@
-package haennihaesseo.sandoll.domain.font.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserFontRepository extends JpaRepository<UserFont, UserFontId> {
-}
